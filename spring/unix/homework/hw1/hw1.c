6ac185fd00b411ce5ceb108dafbf4d7b399889d8
Andres Ponce <andreseeponce@gmail.com>
2021-03-27 01:27:33 +0000

    loop through /proc subdirectories

--- spring/unix/homework/hw1/hw1.c
+++ spring/unix/homework/hw1/hw1.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <dirent.h>
+#include <string.h>
 /*
  * TODO: 
  * 	1. Mess w/ /proc to see needed info.
@@ -9,21 +10,33 @@
  * 	3. For the data not available in stat, how do we find it? Manually parse /proc/pid/? 
  * */
 #include "opts.h"
+#include "entry.h"
 
 char* procPath = "/proc/";
+// since max length is 250
+char procEntryName[251];
 int main(int argc, char *argv[]){
 	struct CmdLineArgs args;
 	parseOptions(&args, argc, argv);	
 
 	// Similar to getopt(), get one dirent at a time
-	DIR *dir;
-	struct dirent *entry;
+	DIR *dir, *subDir;
+	struct dirent *dirEntry;
+	struct dirent *subDirEntry;
+	
+	struct lsofEntry* procInfo;	
 
 	if((dir = opendir(procPath)) == NULL){
 		perror("opendir");	
 	}
-	while((entry= readdir(dir)) != NULL){
-		printf("%s\n", entry->d_name);	
+	// Read each directory in /proc
+	while((dirEntry= readdir(dir)) != NULL){
+		getPathName(procEntryName, procPath, dirEntry);
+		printf("%s\n", procEntryName);
+		if((subDir= opendir(procEntryName)) == NULL){
+			continue;
+		}
+		getProcInfo(procInfo, subDir);
 	}
 	return 0;
 }
