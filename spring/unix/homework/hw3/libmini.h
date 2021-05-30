0eee4608aa4b6d010ff11ca879acc6adb79f6ec5
Andres Ponce <andreseeponce@gmail.com>
2021-05-30 16:30:01 +0000

    added class material

--- spring/unix/homework/hw3/libmini.h
+++ spring/unix/homework/hw3/libmini.h
@@ -1,6 +1,18 @@
 #ifndef LIBMINI_H
 #define LIBMINI_H
 
+typedef long long size_t;
+typedef long long ssize_t;
+typedef long long off_t;
+typedef int mode_t;
+typedef int uid_t;
+typedef int gid_t;
+typedef int pid_t;
+
+extern long errno;
+
+#define	NULL		((void*) 0)
+
 /* From /usr/include/asm-generic/errno-base.h */
 #define	EPERM		 1	/* Operation not permitted */
 #define	ENOENT		 2	/* No such file or directory */
@@ -85,12 +97,168 @@
 #define O_CLOEXEC	02000000	/* set close_on_exec */
 #endif
 
-/* System Calls */
-long sys_exit(int error_code) __attribute__ ((noreturN));
+/* from /usr/include/x86_64-linux-gnu/asm/signal.h */
+#define SIGHUP		 1
+#define SIGINT		 2
+#define SIGQUIT		 3
+#define SIGILL		 4
+#define SIGTRAP		 5
+#define SIGABRT		 6
+#define SIGIOT		 6
+#define SIGBUS		 7
+#define SIGFPE		 8
+#define SIGKILL		 9
+#define SIGUSR1		10
+#define SIGSEGV		11
+#define SIGUSR2		12
+#define SIGPIPE		13
+#define SIGALRM		14
+#define SIGTERM		15
+#define SIGSTKFLT	16
+#define SIGCHLD		17
+#define SIGCONT		18
+#define SIGSTOP		19
+#define SIGTSTP		20
+#define SIGTTIN		21
+#define SIGTTOU		22
+#define SIGURG		23
+#define SIGXCPU		24
+#define SIGXFSZ		25
+#define SIGVTALRM	26
+#define SIGPROF		27
+#define SIGWINCH	28
+#define SIGIO		29
+#define SIGPOLL		SIGIO
+
+#define	SA_NOCLDSTOP  1		 /* Don't send SIGCHLD when children stop.  */
+#define SA_NOCLDWAIT  2		 /* Don't create zombie on child death.  */
+#define SA_SIGINFO    4		 /* Invoke signal-catching function with
+				    three arguments instead of one.  */
+# define SA_ONSTACK   0x08000000 /* Use signal stack by using `sa_restorer'. */
+# define SA_RESTART   0x10000000 /* Restart syscall on signal return.  */
+# define SA_INTERRUPT 0x20000000 /* Historical no-op.  */
+# define SA_NODEFER   0x40000000 /* Don't automatically block the signal when
+				    its handler is being executed.  */
+# define SA_RESETHAND 0x80000000 /* Reset to SIG_DFL on entry to handler.  */
+
+#define	SIG_BLOCK     0		 /* Block signals.  */
+#define	SIG_UNBLOCK   1		 /* Unblock signals.  */
+#define	SIG_SETMASK   2		 /* Set the set of blocked signals.  */
+/* structure required by functions*/
+struct timespec {
+	long	tv_sec;		/* seconds */
+	long	tv_nsec;	/* nanoseconds */
+};
+
+struct timeval {
+	long	tv_sec;		/* seconds */
+	long	tv_usec;	/* microseconds */
+};
+
+struct timezone {
+	int	tz_minuteswest;	/* minutes west of Greenwich */
+	int	tz_dsttime;	/* type of DST correction */
+};
+struct sigset_t{
+
+};
+struct jmp_buf_s {
+	long long reg[8];
+	sigset_t mask;	
+} jmp_buf_s[1];
+
+struct sigaction {
+	void (*sa_handler)(int);
+	void (*sa_sigaction)(int, siginfo_t *, void*);
+	sigset_t sa_mask;
+	int sa_flags;
+	void (*sa_restorer)(void);
+};
+
+struct sigset_t {
+
+};
+/* system calls */
+long sys_read(int fd, char *buf, size_t count);
+long sys_write(int fd, const void *buf, size_t count);
+long sys_open(const char *filename, int flags, ... /*mode*/);
+long sys_close(unsigned int fd);
+long sys_mmap(void *addr, size_t len, int prot, int flags, int fd, off_t off);
+long sys_mprotect(void *addr, size_t len, int prot);
+long sys_munmap(void *addr, size_t len);
+long sys_pipe(int *filedes);
+long sys_dup(int filedes);
+long sys_dup2(int oldfd, int newfd);
+long sys_pause();
+long sys_nanosleep(struct timespec *rqtp, struct timespec *rmtp);
+long sys_fork(void);
+long sys_exit(int error_code) __attribute__ ((noreturn));
+long sys_getcwd(char *buf, size_t size);
+long sys_chdir(const char *pathname);
+long sys_rename(const char *oldname, const char *newname);
+long sys_mkdir(const char *pathname, int mode);
+long sys_rmdir(const char *pathname);
+long sys_creat(const char *pathname, int mode);
+long sys_link(const char *oldname, const char *newname);
+long sys_unlink(const char *pathname);
+long sys_readlink(const char *path, char *buf, size_t bufsz);
+long sys_chmod(const char *filename, mode_t mode);
+long sys_chown(const char *filename, uid_t user, gid_t group);
+long sys_umask(int mask);
+long sys_gettimeofday(struct timeval *tv, struct timezone *tz);
+long sys_getuid();
+long sys_getgid();
+long sys_setuid(uid_t uid);
+long sys_setgid(gid_t gid);
+long sys_geteuid();
+long sys_getegid();
+
+/* wrappers */
+int setjmp(struct jmp_buf_s env);
+void longjmp(struct jmp_buf_s env, int value);
+void (*signal(int sig, void (*func)(int)))(int);
+int sigaction(int signum, const struct sigaction *act, struct sigaction *oldact);
+int sigprocmask(int how, const sigset_t *set, sigset_t *oldset);
+ssize_t	read(int fd, char *buf, size_t count);
+ssize_t	write(int fd, const void *buf, size_t count);
+int	open(const char *filename, int flags, ... /*mode*/);
+int	close(unsigned int fd);
+void *	mmap(void *addr, size_t len, int prot, int flags, int fd, off_t off);
+int	mprotect(void *addr, size_t len, int prot);
+int	munmap(void *addr, size_t len);
+int	pipe(int *filedes);
+int	dup(int filedes);
+int	dup2(int oldfd, int newfd);
+int	pause();
+int	nanosleep(struct timespec *rqtp, struct timespec *rmtp);
+pid_t	fork(void);
+void	exit(int error_code);
+char *	getcwd(char *buf, size_t size);
+int	chdir(const char *pathname);
+int	rename(const char *oldname, const char *newname);
+int	mkdir(const char *pathname, int mode);
+int	rmdir(const char *pathname);
+int	creat(const char *pathname, int mode);
+int	link(const char *oldname, const char *newname);
+int	unlink(const char *pathname);
+ssize_t	readlink(const char *path, char *buf, size_t bufsz);
+int	chmod(const char *filename, mode_t mode);
+int	chown(const char *filename, uid_t user, gid_t group);
+int	umask(int mask);
+int	gettimeofday(struct timeval *tv, struct timezone *tz);
+uid_t	getuid();
+gid_t	getgid();
+int	setuid(uid_t uid);
+int	setgid(gid_t gid);
+uid_t	geteuid();
+gid_t	getegid();
+
+void bzero(void *s, size_t size);
+size_t strlen(const char *s);
+void perror(const char *prefix);
+unsigned int sleep(unsigned int s);
 
 
-/* Wrappers */
-void exit(int);
-unsigned int alarm(unsigned int);
+
 
 #endif
